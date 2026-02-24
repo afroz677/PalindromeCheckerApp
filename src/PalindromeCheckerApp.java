323e22494d50df9ef94fdd60beb5370ab016d109
afroz677 <afrozhussainshaik25@gmail.com>
2026-02-24 14:25:18 +0530

    UC2: Logic Done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System\nVersion 1.0\nSystem initialised successfully");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input text:");
+        String original = sc.nextLine();
+        String reversed = "";
+        int i;
+        for (i=original.length()-1;i>=0;i--)
+        {
+            reversed += original.charAt(i);
+        }
+        System.out.println("Is it Palindrome? ");
+        if (original.equals(reversed))
+            System.out.println("True");
+        else
+            System.out.println("False");
     }
 }
