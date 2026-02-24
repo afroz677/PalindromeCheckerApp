40a6460a4ee4b653d0f9f97dd14ecbe77b306e5c
afroz677 <afrozhussainshaik25@gmail.com>
2026-02-24 15:14:08 +0530

    UC7:Done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,27 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        String input = "civic";
-        Queue<Character> queue = new LinkedList<>();
 
-        Stack<Character> stack = new Stack<>();
+        String input = "refer";
 
-        for(char c : input.toCharArray())
-        {
-            queue.add(c);
-            stack.push(c);
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.add(c);
         }
+
         boolean isPalindrome = true;
 
-        while(!queue.isEmpty())
-        {
-            if(!queue.remove().equals(stack.pop()))
-            {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
         }
+
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? " + isPalindrome);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
